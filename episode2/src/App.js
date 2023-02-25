19efdee68da37a184d4cc6c16ceb75b2eb12563c
choiruladamm <choiruladamm@gmail.com>
2023-02-25 20:52:09 +0700

    JSX, Component, Props...

--- episode2/src/App.js
+++ episode2/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
@@ -10,31 +11,57 @@ function App() {
       <Job salary="999999" position="Software Enginer" company="Netlix" />
       <Job salary="888888" position="Front End" company="Tokopedia" /> */}
       <Comments {...dataComments} />
+      <Home room="Room Axel" clothes="Uniqlo" smartphone="Iphone 14 Pro Max" />
+      <MyButton />
+      <MyButton />
     </div>
   );
 }
 
-const User = (props) => {
+const MyButton = () => {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+  return (
+    <button className="btn " onClick={handleClick}>
+      <span>Clicked {count} times</span>
+    </button>
+  );
+};
+
+// const User = (props) => {
+//   return (
+//     <div>
+//       <h1>{props.name}</h1>
+//       <h1>{props.age}</h1>
+//       <h1>{props.email}</h1>
+//     </div>
+//   );
+// };
+
+// const Job = (props) => {
+//   return (
+//     <div>
+//       <h1>{props.salary}</h1>
+//       <h1>{props.position}</h1>
+//       <h1>{props.company}</h1>
+//     </div>
+//   );
+// };
+
+const Home = (props) => {
   return (
     <div>
-      <h1>{props.name}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.email}</h1>
+      <h1>{props.room}</h1>
+      <h1>{props.clothes}</h1>
+      <h1>{props.smartphone}</h1>
     </div>
   );
 };
 
-const Job = (props) => {
-  return (
-    <div>
-      <h1>{props.salary}</h1>
-      <h1>{props.position}</h1>
-      <h1>{props.company}</h1>
-    </div>
-  );
-};
-
-const FormatDate = (date) => {
+const cormatDate = (date) => {
   return date.toLocaleDateString();
 };
 
@@ -42,7 +69,8 @@ const Comments = (props) => {
   return (
     <div>
       <img src={props.author.img} alt={props.author.name}></img>
-      <h1>{FormatDate(props.date)}</h1>
+      <h3>{props.author.name}</h3>
+      <h1>{cormatDate(props.date)}</h1>
     </div>
   );
 };
