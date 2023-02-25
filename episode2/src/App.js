d88d4a6878d45518f43cc2926a3b147e4c10894e
choiruladamm <choiruladamm@gmail.com>
2023-02-25 16:48:37 +0700

    JSX, Component, Props...

--- episode2/src/App.js
+++ episode2/src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 
 function App() {
-  const names = "Adam";
-  return <div className="App">
-    <h1>Hello {names}</h1>
-  </div>;
+  return (
+    <div className="App">
+      <User name="James" age={21} email="james@james.com" />
+      <User name="Axell" age={21} email="axell@axell.com" />
+      <User name="Adam" age={21} email="adam@adam.com" />
+    </div>
+  );
 }
+
+const User = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h1>{props.email}</h1>
+    </div>
+  );
+};
 
 export default App;
