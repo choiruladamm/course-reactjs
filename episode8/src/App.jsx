7a35d44d35d5fbe16b718b9a94761190f9e25b7b
choiruladamm <choiruladamm@gmail.com>
2023-03-08 13:45:25 +0700

    Routes React using React Router Dom

--- episode8/src/App.jsx
+++ episode8/src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
+import { Menu } from "./pages/Menu";
+import { Error } from "./pages/Error";
+import { Navbar } from "./Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ episode8/src/pages/Error.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Error = () => {
+  return <h1>KAMU TERSESAT OII</h1>;
+};
